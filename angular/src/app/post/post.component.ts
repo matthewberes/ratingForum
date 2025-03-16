3d695383bb37e4847bb03bb62ecdf51e458fda91
matthewberes <matthewberes@trentu.ca>
2025-03-16 17:45:58 -0400

    format text post

--- angular/src/app/post/post.component.ts
+++ angular/src/app/post/post.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIf],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
 export class PostComponent {
-
+  image: boolean = false;
 }
