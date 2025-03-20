3b37ed7747436bc12c7c6673a7068b7325c5caea
matthewberes <matthewberes@trentu.ca>
2025-03-20 15:03:45 -0400

    add sidenav skeleton

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostListComponent, HeaderComponent],
+  imports: [RouterOutlet, PostListComponent, HeaderComponent, SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
